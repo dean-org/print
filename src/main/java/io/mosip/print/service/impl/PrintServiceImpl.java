382afd12d5c1234293e509b18ab214b8f701f620
Dharanidharan-S-04 <dean12345.dd@gmail.com>
2026-02-12 19:48:08 +0530

    Update PrintServiceImpl.java

--- src/main/java/io/mosip/print/service/impl/PrintServiceImpl.java
+++ src/main/java/io/mosip/print/service/impl/PrintServiceImpl.java
@@ -333,6 +333,7 @@ public class PrintServiceImpl implements PrintService {
         String template = UIN_CARD_TEMPLATE;
         byte[] pdfBytes = null;
         String templateLang = null;
+		String preferredLang = null;
 
         try {
 
@@ -391,6 +392,7 @@ public class PrintServiceImpl implements PrintService {
             String prefLangAttr = (String) attributes.get(userPreferredLanguageAttribute);
             printLogger.info("userPreferredLanguageAttribute: {}, prefLangAttr: {}, languageCodes: {}", userPreferredLanguageAttribute, prefLangAttr, languageCodes);
             if (prefLangAttr != null && languageCodes != null && !languageCodes.isEmpty()) {
+				preferredLang = prefLangAttr;
                 templateLang = (String) languageCodes.get(Base64.encodeBase64String(prefLangAttr.getBytes()));
             }
             if (!StringUtils.hasText(templateLang)) {
@@ -605,7 +607,7 @@ public class PrintServiceImpl implements PrintService {
         try {
             List<String> emailIds = Arrays.asList(residentEmailId, defaultEmailIds);
             List<NotificationResponseDTO>  notificationResponseDTOs = notificationUtil.emailNotification(emailIds,null,
-                    ACCT_EMAIL, ACCT_EMAIL_SUB, attributes, null, templateLang);
+                    ACCT_EMAIL, ACCT_EMAIL_SUB, attributes, null, preferredLang);
             notificationResponseDTOs.forEach(responseDTO ->
                 printLogger.info("Account creation notification sent successfully via Email, server response..{}", responseDTO)
             );
@@ -620,7 +622,7 @@ public class PrintServiceImpl implements PrintService {
             try {
                 List<String> emailIds = Arrays.asList(residentEmailId, defaultEmailIds);
                 List<NotificationResponseDTO> responseDTOs = notificationUtil.emailNotification(emailIds, fileName,
-                        UIN_CARD_EMAIL, UIN_CARD_EMAIL_SUB, attributes, pdfbytes, templateLang);
+                        UIN_CARD_EMAIL, UIN_CARD_EMAIL_SUB, attributes, pdfbytes, preferredLang);
                 responseDTOs.forEach(responseDTO ->
                         printLogger.info("UIN sent successfully via Email, server response..{}", responseDTO)
                 );
